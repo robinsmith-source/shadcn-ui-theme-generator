4a7dcbf827bb36d0ceb8566422dd4c6fc18c8c08
Robin Schmidt <schmidtrobin02@gmail.com>
2024-02-17 01:35:04 +0100

    feat: make copy button work

--- src/components/misc/copy-button.tsx
+++ src/components/misc/copy-button.tsx
@@ -8,16 +8,27 @@ import {
 } from "@/components/ui/dialog.tsx";
 import {Button} from "@/components/ui/button.tsx";
 import {CheckIcon, CopyIcon} from "lucide-react";
-import {useState} from "react";
+import {useGeneratedTheme} from "@/lib/generatedThemeContext.ts";
+import {ScrollArea} from "@/components/ui/scroll-area.tsx";
+import {useCopyToClipboard} from "usehooks-ts";
+
 
 export default function CopyButton() {
-    const [hasCopied, setHasCopied] = useState(false)
+    const {theme} = useGeneratedTheme()
+    const cssCode = '@layer base ' + JSON.stringify({
+        ':root': theme?.light,
+        'dark': theme?.dark,
+    }, null, '\t').replace(/"/g, '')
+
+ const [copiedText, copyToClipboard] = useCopyToClipboard();
+
+
     return (
         <Dialog>
             <DialogTrigger asChild>
-                <Button className="hidden md:flex">Copy code</Button>
+                <Button>Copy code</Button>
             </DialogTrigger>
-            <DialogContent className="max-w-2xl outline-none">
+            <DialogContent className="max-w-2xl max-h-[90vh] outline-none">
                 <DialogHeader>
                     <DialogTitle>Theme</DialogTitle>
                     <DialogDescription>
@@ -28,19 +39,26 @@ export default function CopyButton() {
                     <Button
                         size="sm"
                         onClick={async () => {
-
-                            setHasCopied(true)
+                            await copyToClipboard(cssCode)
                         }}
                         className="bg-muted text-muted-foreground hover:bg-muted hover:text-muted-foreground"
                     >
-                        {hasCopied ? (
+                        {copiedText ? (
                             <CheckIcon className="mr-2 h-4 w-4"/>
                         ) : (
                             <CopyIcon className="mr-2 h-4 w-4"/>
                         )}
                         Copy
                     </Button>
-                    {/*<code className="text-sm p-4 leading-normal">{themeCode}</code>*/}
+                    <ScrollArea className="h-[28rem] rounded-md border p-4">
+                    <div className="p-2 bg-muted rounded-md">
+                        <pre className="text-sm">
+                            <code>
+                            {cssCode}
+                            </code>
+                        </pre>
+                    </div>
+                        </ScrollArea>
                 </div>
             </DialogContent>
         </Dialog>)
